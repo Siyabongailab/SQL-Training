dd5f355d4f7d0cf2dd0e1fb3a2de331165988e28
Siyabonga <Siyabonga.Sithole@ilabqa.com>
2024-11-18 14:45:18 +0200

    exercise 5, comparison operators

---
+++ Exercise_5.sql
@@ -0,0 +1,16 @@
+--1
+
+select Employeeid, City 
+from  Employees
+where country = 'UK';
+
+--2
+select ProductName, UnitsInStock 
+from   Products
+where  UnitsInStock < 17;
+
+--3
+
+select  UnitPrice, ProductName, Productid
+from    Products
+where   UnitPrice <=30;
