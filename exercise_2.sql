8c7d7bfb07b2c2af7471ad1014aeb83f5bffb4c3
Siyabonga <Siyabonga.Sithole@ilabqa.com>
2024-11-18 13:46:01 +0200

    exercise 3, order by

---
+++ exercise_2.sql
@@ -0,0 +1,11 @@
+
+--1
+select distinct title from Employees;
+
+--2 
+
+select distinct country from Suppliers;
+
+--3
+
+select distinct productid from [order details];
---
+++ exercise_3.sql
@@ -0,0 +1,9 @@
+
+--1 
+select distinct ShipCity, ShipCountry from Orders
+order by 2;
+
+--2
+select distinct ShipCity, ShipCountry from Orders
+order by 1 desc;
+
