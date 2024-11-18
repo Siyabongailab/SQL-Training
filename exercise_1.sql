f1782058cd2d4d6a2ae838e6d15ad5e4dfbfc0c4
Siyabonga <Siyabonga.Sithole@ilabqa.com>
2024-11-18 11:39:08 +0200

    exercise 1, queries

---
+++ exercise_1.sql
@@ -0,0 +1,14 @@
+--1
+select * from  Suppliers;
+
+--2
+select orderID, OrderDate, RequiredDate, CustomerID, EmployeeID from Orders;
+
+--3
+select FirstName, LastName, EmployeeID from Employees;
+
+--4
+select RegionDescription from Region;
+
+--5
+select  CategoryID, CategoryName, [Description] from Categories;
